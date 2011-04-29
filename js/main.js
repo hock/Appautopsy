04acb71575a8c31ce1d89efaf1ee4a6e6fd3b799
Matthew Hockenberry <hock@media.mit.edu>
2011-04-29 18:49:33 -0400

    error checking

--- js/main.js
+++ js/main.js
@@ -1,42 +1,63 @@
 $(document).ready(function() {
-    var cubeMarkup = '<div id="${ID}" class="cube ${Color}" style="z-index:${Index};"> <span class="title">${Title}</span><span class="link">${Link}</span><div class="topface"><div style="background-image:url(${BGImage});"><h1>${Title}</h1></div></div> <div class="leftface"><div></div></div> <div class="rightface"><div></div></div> <div class="label"><strong><a href="${Link}">${Title}</a><br/><em>${Details}</em> ${Description}</div> </div>';
+    var cubeMarkup = '<div id="${ID}" class="cube ${Color}" style="z-index:${Index};"> <span class="title">${Title}</span><span class="link">${Link}</span><div class="topface"><div style="background-image:url(${BGImage});"></div></div> <div class="leftface"><div></div></div> <div class="rightface"><div></div></div> <div class="label"><strong><a href="${Link}">${Title}</a><br/><em>${Details}</em> ${Description}</div> </div>';
     var overviewMarkup = '<img class="over-badge" src="${BGImage}"/><h1><a href="${Link}">${Title}</a></h1><p>${Description}</p><div id="geocode"></div>';
     $.template( "cubeTemplate", cubeMarkup );
     $.template( "overviewTemplate", overviewMarkup );
     var components = [];        
     
     
+    $('#appaddress').bind('keypress', function(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if(code == 13) { $("#autopsyaction").click(); }
+    });
+    
     $("#autopsyaction").click(function() {
         if($("#appaddress").val() == "") {
            $("#appaddress").val(startaddress); 
         }
+        $("#appaddress").val($("#appaddress").val().replace("http://","").replace("www.",""));
+        
+
         $.ajax({
             url: 'http://appautopsy.com/service/index.php?s='+$("#appaddress").val(), 
             dataType: 'jsonp', 
             success: function(data){
-               for(var d in data.plugins) {
-                   var strings = ""; var modules = "";
-                   if(typeof(data.plugins[d].string) != 'undefined') {
-                       strings = data.plugins[d].string.join();
-                   }
-                   if(typeof(data.plugins[d].module) != 'undefined') {
-                       modules = data.plugins[d].module.join();
-                   }
-                   data.plugins[d].name = d;
-                   data.plugins[d].details = strings + modules;
-                   data.plugins[d].description = data.plugins[d].description.join();               
-
-                }
-                data.title = data.plugins["Title"].details;
-                loadcontent(data);
-                $.getJSON("geoloc.php?s="+data.plugins["IP"].details, function(loc) {
-                    console.log(loc);
+                if(typeof(data) == "undefined") {
+                    alert("We couldn't figure out that url (try it without www?)");
+                } else {
+                    if(typeof(data.plugins) != 'undefined') {
+                       for(var d in data.plugins) {
+                           var strings = ""; var modules = "";
+                           if(typeof(data.plugins[d].string) != 'undefined') {
+                               strings = data.plugins[d].string.join();
+                           }
+                           if(typeof(data.plugins[d].module) != 'undefined') {
+                               modules = data.plugins[d].module.join();
+                           }
+                           data.plugins[d].name = d;
+                           data.plugins[d].details = strings + modules;
+                           if(typeof(data.plugins[d].description) != 'undefined') {
+                               data.plugins[d].description = data.plugins[d].description.join();               
+                            }
+                        } // end for
+                    }
+                    if(typeof(data.plugins) != 'undefined' 
+                        && typeof(data.plugins["Title"]) != 'undefined') {
+                        data.title = data.plugins["Title"].details;
+                    }
+                    loadcontent(data);
+                            if(typeof(data.plugins) != 'undefined' 
+                                && typeof(data.plugins["Title"]) != 'undefined') {
+                        $.getJSON("geoloc.php?s="+data.plugins["IP"].details, function(loc) {
+                            console.log(loc);
                     
-                    console.log(loc["latitude"]);
-                    var bgimage = "http://staticmaps.cloudmade.com/8ee2a50541944fb9bcedded5165f09d9/staticmap?center="+loc.latitude+","+loc.longitude+"&zoom=8&size=500x300&format=png&styleid=999";
-                    console.log(bgimage);
-                    $("#geocode").css("background-image", "url('"+bgimage+"')");
-                });
+                            console.log(loc["latitude"]);
+                            var bgimage = "http://staticmaps.cloudmade.com/8ee2a50541944fb9bcedded5165f09d9/staticmap?center="+loc.latitude+","+loc.longitude+"&zoom=8&size=500x300&format=png&styleid=999";
+                            console.log(bgimage);
+                            $("#geocode").css("background-image", "url('"+bgimage+"')");
+                        });
+                    }
+                }
             }
         }); 
     });
